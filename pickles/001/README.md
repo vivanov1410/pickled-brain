5114eb13608d1fd31ce8cc6f35794850f3157f5c
Viacheslav Ivanov <slava.eth@gmail.com>
2014-01-30 14:47:44 -0800

    added pickle001

---
+++ pickles/001/README.md
@@ -0,0 +1,26 @@
+# 001 Calculator. The Beginning
+
+## Background
+You are assigned to write your own calculator class for a new project. For now you decided to write addition and subtraction functions.
+
+## Pickle
+- create a "add" function that adds two numbers so
+```python
+print add(1, 5) # 6
+```
+
+- create a "subtract" function that subtracts two numbers
+```python
+print subtract(10, 14) # -4
+```
+
+## Solution
+In /solutions make copy a /default folder and rename it with you name. Modify pickle001.py file to submit a solution.
+
+## Testing
+In your solution folder run following command to test it:
+```
+> python tests.py
+```
+
+If all tests are passed the you did a good work.
---
+++ pickles/001/solutions/default/tests.py
@@ -0,0 +1,120 @@
+import unittest
+
+from pickle001 import add, subtract, multiply, divide
+
+
+class AddTest(unittest.TestCase):
+    """Test for add() function"""
+
+    def test_add_positive_numbers(self):
+        self.assertEquals(add(1, 2), 3)
+
+    def test_add_positive_numbers2(self):
+        self.assertEquals(add(10, 25), 35)
+
+    def test_add_zeros(self):
+        self.assertEquals(add(0, 0), 0)
+
+    def test_add_positive_and_zero(self):
+        self.assertEquals(add(45, 0), 45)
+
+    def test_add_zero_and_positive(self):
+        self.assertEquals(add(0, 45), 45)
+
+    def test_add_negative_and_zero(self):
+        self.assertEquals(add(-33, 0), -33)
+
+    def test_add_zero_and_negative(self):
+        self.assertEquals(add(0, -33), -33)
+
+    def test_add_negative_numbers(self):
+        self.assertEquals(add(-3, -15), -18)
+
+    def test_add_negative_numbers2(self):
+        self.assertEquals(add(-101, -19), -120)
+
+    def test_add_positive_and_negative(self):
+        self.assertEquals(add(21, -7), 14)
+
+    def test_add_negative_and_positive(self):
+        self.assertEquals(add(-7, 21), 14)
+
+
+class SubtractTest(unittest.TestCase):
+    """Test for subtract() function"""
+
+    def test_subtract_positive_numbers(self):
+        self.assertEquals(subtract(10, 6), 4)
+
+    def test_subtract_positive_numbers2(self):
+        self.assertEquals(subtract(111, 11), 100)
+
+    def test_subtract_zeros(self):
+        self.assertEquals(subtract(0, 0), 0)
+
+    def test_subtract_positive_and_zero(self):
+        self.assertEquals(subtract(45, 0), 45)
+
+    def test_subtract_zero_and_positive(self):
+        self.assertEquals(subtract(0, 45), -45)
+
+    def test_subtract_negative_and_zero(self):
+        self.assertEquals(subtract(-33, 0), -33)
+
+    def test_subtract_zero_and_negative(self):
+        self.assertEquals(subtract(0, -33), 33)
+
+    def test_subtract_negative_numbers(self):
+        self.assertEquals(subtract(-3, -15), 12)
+
+    def test_subtract_negative_numbers2(self):
+        self.assertEquals(subtract(-79, -19), -60)
+
+    def test_subtract_positive_and_negative(self):
+        self.assertEquals(subtract(21, -7), 28)
+
+    def test_subtract_negative_and_positive(self):
+        self.assertEquals(subtract(-7, 21), -28)
+
+class MultiplyTest(unittest.TestCase):
+    """Test for multiply() function"""
+
+    def test_multiply_positive_numbers(self):
+        self.assertEquals(multiply(10, 6), 60)
+
+    def test_multiply_positive_and_negative(self):
+        self.assertEquals(multiply(10, -6), -60)
+
+    def test_multiply_negative_numbers(self):
+        self.assertEquals(multiply(-10, -6), 60)
+
+    def test_multiply_zeros(self):
+        self.assertEquals(multiply(0, 0), 0)
+
+    def test_multiply_positive_and_zero(self):
+        self.assertEquals(multiply(10, 0), 0)
+
+    def test_multiply_negative_and_zero(self):
+        self.assertEquals(multiply(-10, 0), 0)
+
+class DivideTest(unittest.TestCase):
+    """Test for divide() function"""
+
+    def test_divide_positive_numbers(self):
+        self.assertEquals(divide(60, 6), 10)
+
+    def test_divide_negative_numbers(self):
+        self.assertEquals(divide(-60, -6), 10)
+
+    def test_divide_positive_and_negative(self):
+        self.assertEquals(divide(60, -6), -10)
+
+    def test_divide_zero_by_number(self):
+        self.assertEquals(divide(0, 123), 0)
+
+    def test_divide_number_by_zero(self):
+        self.assertEquals(divide(10, 0), 'error')
+
+
+if __name__ == '__main__':
+    unittest.main()
