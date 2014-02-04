e4a65fdb02bd3d3e52051374c00f5fefcdb9fe04
Viacheslav Ivanov <slava.eth@gmail.com>
2014-02-04 01:13:09 -0800

    added juice to my solution

--- 004/vivanov/pickle004.py
+++ 004/vivanov/pickle004.py
@@ -1,4 +1,43 @@
 from random import randrange
+from time import sleep
+
+
+class App:
+    """Main class for our app"""
+    def __init__(self, name):
+        self.app_name = name
+        self.welcome()
+
+    def welcome(self):
+        print('Welcome to {0}! First python app from Svetik :)'.format(self.app_name))
+        print('Picker will help you make a hard choice by choosing a movie for you')
+        print('')
+
+    def run(self):
+        movies = raw_input('Enter movies(separated by comma): ').split(',')
+
+        if movies[0] == '':
+            print('Very interesting... Then nothing indeed!')
+            exit()
+
+        print(self.hard())
+
+        print('')
+        print('Thinking. Answer will be ready in')
+        for x in range(3, 0, -1):
+            print('{0} sec'.format(x))
+            sleep(1)
+
+        picker = MoviePicker(movies)
+        pick = picker.pick()
+        print('')
+        print('Finally! Today you should watch movie: {0}'.format(pick))
+        print('Enjoy!')
+
+    def hard(self):
+        hard_phrases = ['Wow, thats hard...', 'I need to really think about it...', 'This is way too hard...', 'Why you treat me like that...', 'Love ya...']
+        index = randrange(len(hard_phrases))
+        return hard_phrases[index]
 
 
 class MoviePicker:
@@ -20,7 +59,5 @@ class MoviePicker:
         choice = randrange(len(self.movies))
         return self.movies[choice]
 
-my_movies = raw_input('Enter movies(separated by comma): ').split(',')
-picker = MoviePicker(my_movies)
-picker.add('i love you')
-print(picker.pick())
+app = App('Movie Picker')
+app.run()
